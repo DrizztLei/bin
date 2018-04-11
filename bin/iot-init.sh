5ae5ce7746ca7a3b6df81b276482f2c25841828c
DrizztLei <elvis.linuxer@gmail.com>
2018-04-11 18:48:32 +0800

    updata in Wed Apr 11 18:48:32 CST 2018.

--- bin/iot-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-node ~/environment/iot-sol/node_modules/hope-http-broker/bin/start_broker 16666 &
-node ~/environment/iot-sol/node_modules/hope-center/center ~/environment/iot-sol/node_modules/hope-demo/center/config.json &
--- bin/phpmyadmin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo /usr/sbin/webapp-config -h localhost -u root -d /phpmyadmin -I phpmyadmin 4.6.4
